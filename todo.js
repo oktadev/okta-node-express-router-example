835b1766cdcaf02eb281defb3cfcb237767085a8
Braden M. Kelley <redbmk@gmail.com>
2018-10-22 12:48:21 -0700

    have a different todo list for each user

--- todo.js
+++ todo.js
@@ -2,17 +2,21 @@ const express = require('express')
 
 const router = express.Router()
 
-let todo = []
+const todosByUser = {}
 
 router.post('/', (req, res, next) => {
-  todo = [...req.body.todo || []]
+  const todo = [...req.body.todo || []]
   if (req.body.remove) todo.splice(req.body.remove, 1)
   if (req.body.new) todo.push({})
+
+  todosByUser[req.userContext.userinfo.sub] = todo
 
   next()
 })
 
 router.use('/', (req, res) => {
+  const todo = todosByUser[req.userContext.userinfo.sub] || []
+
   res.render('todo', { title: 'To-do list', todo, userinfo: req.userContext.userinfo })
 })
 
